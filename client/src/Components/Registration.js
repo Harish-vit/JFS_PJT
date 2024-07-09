ff7d3be1de34663909bfde9e62eafd2d2eeb0e79
Akshitha2003 <akshithamaryac@gmail.com>
2024-07-10 02:43:13 +0530

    Added styling for registration form

--- client/src/Components/Registration.js
+++ client/src/Components/Registration.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './Registration.css'
 
 const Registration = () => {
     const navigate = useNavigate();
@@ -39,83 +40,85 @@ const Registration = () => {
     };
 
     return (
-        <div>
-            <h2>Register</h2>
-            <form onSubmit={handleRegister}>
-                <label>
-                    Name:
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Email:
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Username:
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Password:
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Age:
-                    <input
-                        type="number"
-                        value={age}
-                        onChange={(e) => setAge(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Height (cm):
-                    <input
-                        type="number"
-                        value={height}
-                        onChange={(e) => setHeight(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Weight (kg):
-                    <input
-                        type="number"
-                        value={weight}
-                        onChange={(e) => setWeight(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Gender:
-                    <select value={gender} onChange={(e) => setGender(e.target.value)} required>
-                        <option value="" selected disabled>Select</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                    </select>
-                </label>
-                <button type="submit">Register</button>
-            </form>
+        <div className='register-container'>
+            <h2>Register Here!!!</h2>
+            <div className='form-container'>
+                <form onSubmit={handleRegister}>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Username:
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Age:
+                        <input
+                            type="number"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Height (cm):
+                        <input
+                            type="number"
+                            value={height}
+                            onChange={(e) => setHeight(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Weight (kg):
+                        <input
+                            type="number"
+                            value={weight}
+                            onChange={(e) => setWeight(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        Gender:
+                        <select value={gender} onChange={(e) => setGender(e.target.value)} required>
+                            <option value="" disabled>Select</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </select>
+                    </label>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
         </div>
     )
 };
