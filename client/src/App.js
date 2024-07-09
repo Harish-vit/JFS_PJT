b32fd176c19c8003aed2642cf0bdfa884820c4b0
Harish-vit <jgharish12a@gmail.com>
2024-07-09 20:15:23 +0530

    Added Back Button

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Layout from './Layout';
 import Login from './Components/Login';
 import Registration from './Components/Registration';
 import FitnessHistory from './Components/FitnessHistory';
 import FitnessForm from './Components/FitnessForm';
-import './App.css'
+import './App.css';
 
 function App() {
+    const [activities, setActivities] = useState([]);
+
+    const handleFormSubmit = (newActivity) => {
+        setActivities(prevActivities => [...prevActivities, newActivity]);
+    };
+
     return (
         <Router>
             <div>
                 <Routes>
-                    <Route exact 
-                        path="/" 
-                        element={
-                            <header>
-                                <h1>Fitness Tracker App</h1>
-                                <Login />
-                            </header>
-                        } 
-                    />
-                    <Route
-                        path="/register" 
-                        element={
-                            <header>
-                                <h1>Fitness Tracker App</h1>
-                                <Registration />
-                            </header>
-                        } 
-                    />
-                    <Route 
-                        path="/history" 
-                        element={
-                            <Layout> 
-                                <FitnessHistory />
-                            </Layout>
-                        } 
-                    />
-                    <Route 
-                        path="/add-activity" 
-                        element={
-                            <Layout>
-                                <FitnessForm />
-                            </Layout>
-                        } 
-                    />
+                    <Route exact path="/" element={
+                        <header>
+                            <h1>Fitness Tracker App</h1>
+                            <Login />
+                        </header>
+                    } />
+                    <Route path="/register" element={
+                        <header>
+                            <h1>Fitness Tracker App</h1>
+                            <Registration />
+                        </header>
+                    } />
+                    <Route path="/history" element={
+                        <Layout>
+                            <FitnessHistory activities={activities} />
+                        </Layout>
+                    } />
+                    <Route path="/add-activity" element={
+                        <Layout>
+                            <FitnessForm onSubmit={handleFormSubmit} />
+                        </Layout>
+                    } />
                 </Routes>
             </div>
         </Router>
--- client/src/Components/FitnessHistory.js
+++ client/src/Components/FitnessHistory.js
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './FitnessHistory.css';
 import { useNavigate } from 'react-router-dom';
 
-function FitnessHistory() {
-    const navigate = useNavigate()
-    const [records, setRecords] = useState([{date:'09.04.2024', duration: '60', workoutType: 'Running', intensity: 'medium'}]);
+function FitnessHistory({ activities }) {
+    const navigate = useNavigate();
 
     const handleAddActivity = () => {
         navigate('/add-activity');
-    };
-
-    const onDelete = (index) => {
-        const newRecords = [...records];
-        newRecords.splice(index, 1);
-        setRecords(newRecords);
-    };
-
-    const onEdit = (index) => {
-        // Implement the update logic here
     };
 
     return (
@@ -25,29 +14,23 @@ function FitnessHistory() {
             <div className='ActivityHeader'>
                 <h2>Your Activities</h2>
                 <div className='addButton'>
-                    <button onClick= { handleAddActivity } >
+                    <button onClick={handleAddActivity}>
                         Add Activity
                     </button>
                 </div>
             </div>
             <ul style={{ padding: '0px' }}>
-                {records.map((record, index) => (
+                {activities.map((record, index) => (
                     <div key={index} className='ActivityContainer'>
                         <p className="activityName">{record.workoutType}</p>
                         <div className="details">
-                            <span>Calories</span>
-                            <span>{record.duration} minutes</span>
-                            <span>{record.intensity}</span>
-                            <span className="date">{record.date}</span>
-                        </div>
-                        <div className="buttons">
-                            <button className="edit" onClick={() => onEdit(index)}>Edit</button>
-                            <button className="delete" onClick={() => onDelete(index)}>Delete</button>
+                            <span>Duration: {record.duration} minutes</span>
+                            <span>Intensity: {record.intensity}</span>
+                            <span className="date">Date: {record.date}</span>
                         </div>
                     </div>
                 ))}
             </ul>
-
         </div>
     );
 }
